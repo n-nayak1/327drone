a1131da2af10e90f2df11432b04ddf0a57a3ced2
Lucas Lanting <gregorylanting2026@u.northwestern.edu>
2025-06-08 19:14:27 -0500

    Main updated

--- src/main.c
+++ src/main.c
@@ -5,11 +5,23 @@
 #include "filter.h"
 #include "rc_control.h"
 
+float DesiredAngleRoll = 0;
+float DesiredAnglePitch = 0;
+int InputThrottle = 0;
+float DesiredRateYaw = 0;
 
 void app_main(void) {
     setup_gpio_interrupts();
-    
+
     while (1) {
+        DesiredAngleRoll  = 0.1f * (ReceiverValue[0] - 1500);
+        DesiredAnglePitch = 0.1f * (ReceiverValue[1] - 1500);
+        InputThrottle     = ReceiverValue[2];
+        DesiredRateYaw    = 0.15f * (ReceiverValue[3] - 1500);
+
+        printf("Roll: %.2f  Pitch: %.2f  Throttle: %d  YawRate: %.2f\n",
+               DesiredAngleRoll, DesiredAnglePitch, InputThrottle, DesiredRateYaw);
+
         mpu6050_update();
         const imu_data_t* imu = mpu6050_get_data();
 
--- src/rc_control.h
+++ src/rc_control.h
@@ -1,1 +1,9 @@
-void setup_gpio_interrupts();
+#pragma once
+
+#include <stdint.h>
+
+#define NUM_CHANNELS 6
+
+extern volatile int ReceiverValue[NUM_CHANNELS];
+
+void setup_gpio_interrupts(void);
